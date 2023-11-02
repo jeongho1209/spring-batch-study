d8eedf3949a2ca204cac561aeca28d7ab701d542
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-02 20:05:58 +0900

    add :: @EnableScheduling

--- src/main/kotlin/com/study/springbatch/SpringBatchStudyApplication.kt
+++ src/main/kotlin/com/study/springbatch/SpringBatchStudyApplication.kt
@@ -3,7 +3,11 @@ package com.study.springbatch
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+internal const val BASE_PACKAGE = "com.study.springbatch"
+
+@EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication
 class SpringBatchStudyApplication
